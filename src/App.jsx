8aef7d98c073c27a748e71f3cfeaadb24f3069c2
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-11-02 11:01:34 +0100

    connected server with client using an api call.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const get = async (endpoint, options) => {
+		const res = await axios.get(endpoint, options)
+		console.log(res.data)
+		return res.data
+	}
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	useEffect(() => {
+		const data = get('http://localhost:3000/api')
+	}, [])
+	return (
+		<div className='App'>
+			<h2>Welcome to my app</h2>
+		</div>
+	)
 }
 
 export default App
