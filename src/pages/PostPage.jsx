aae3f49e008dca5ac851b31f965facaac06b3e71
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-11-09 13:35:24 +0100

    styling for blog post page on mobile and desktop.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -3,15 +3,16 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import Container from 'react-bootstrap/Container'
 import Posts_API from '../services/Posts_API'
-import Post from '../components/Post'
 import { useQueryClient } from 'react-query'
 
 const PostPage = () => {
 	const queryClient = useQueryClient()
 	const { postId } = useParams()
-	const { isLoading, isError, data } = useQuery(['post', postId], () =>
-		Posts_API.showPost(postId)
-	)
+	const {
+		isLoading,
+		isError,
+		data: post,
+	} = useQuery(['post', postId], () => Posts_API.showPost(postId))
 
 	const onSubmit = async data => {
 		if (data) {
@@ -28,11 +29,25 @@ const PostPage = () => {
 	}
 
 	return (
-		<Container>
-			<h1 className='mb-5'>Post {postId}</h1>
-			<div>
-				{data && !isLoading && <Post key={data.id} post={data} />}
-			</div>
+		<Container className='post-page'>
+			{post && (
+				<>
+					<div className='header-container-post-page'>
+						<h1>{post.title}</h1>
+						{post.created_at && (
+							<h2>
+								written by @randomuser on{' '}
+								{new Date(post.created_at).toLocaleString()}
+							</h2>
+						)}
+					</div>
+
+					<p className='post-content'>{post.content}</p>
+					{/* <div>
+				{post && !isLoading && <Post key={post.id} post={post} />}
+			</div> */}
+				</>
+			)}
 		</Container>
 	)
 }
