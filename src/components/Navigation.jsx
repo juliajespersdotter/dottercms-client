7a33012314056fcadcb85c9f4ccbb47d1d470f1f
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-11-09 14:05:15 +0100

    navbar added fontawesome icons and mobile responsive ness.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faMagnifyingGlass,
+	faArrowTrendUp,
+	faCirclePlus,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Navigation = () => {
 	return (
 		<div className='centered-container'>
 			<nav id='navbar'>
 				<div className='links'>
-					<a href=''>User</a>
-					<a href=''>Search</a>
-					<a href=''>Trending</a>
+					<a href=''>
+						<div id='circle'>
+							<span>J</span>
+						</div>
+					</a>
+					<a href=''>
+						<div className='icon'>
+							<FontAwesomeIcon icon={faMagnifyingGlass} />
+						</div>
+						<div className='link-text'>search</div>
+					</a>
+					<a href=''>
+						<div className='icon'>
+							<FontAwesomeIcon icon={faArrowTrendUp} />
+						</div>
+						<div className='link-text'>trending</div>
+					</a>
 					<a className='alone-link' href=''>
-						Create
+						<div className='icon'>
+							<FontAwesomeIcon icon={faCirclePlus} />
+						</div>
+						<div className='link-text'>create</div>
 					</a>
 				</div>
 			</nav>
